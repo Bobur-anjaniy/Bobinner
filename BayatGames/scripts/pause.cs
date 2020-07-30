642eec4cb8a10c64857923f68610f4466001d132
Bobur-anjaniy <65465213+Bobur-anjaniy@users.noreply.github.com>
2020-07-30 20:16:46 +0500

    xamma lvl o'zgargan

---
+++ BayatGames/scripts/pause.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pause : MonoBehaviour
+{
+  
+    
+    public GameObject GameCant;
+    public GameObject PauseMenu;
+    public static bool GameIsPause = false;
+   
+    
+   
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+  
+    public void pause1 ()
+    {
+        GameCant.SetActive(false);
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void Play ()
+    {
+        GameCant.SetActive(true);
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void restart ()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        int ActiveScene = scene.buildIndex;
+        Time.timeScale = 1f;
+        // activ senani int ga o'tkizadi.(shartmas)
+        SceneManager.LoadScene(scene.buildIndex);
+    }
+    public void Home ()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+    }
+}
